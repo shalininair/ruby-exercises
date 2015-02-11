761db0c2ec1c007615859d4d564cef959e823e14
Shalini Nair <shalininair75@gmail.com>
2015-02-11 09:07:09 +0530

    ruby koans - about scope

--- koans/about_scope.rb
+++ koans/about_scope.rb
@@ -18,7 +18,7 @@ class AboutScope < Neo::Koan
   end
 
   def test_dog_is_not_available_in_the_current_scope
-    assert_raise(___) do
+    assert_raise(NameError) do
       Dog.new
     end
   end
@@ -26,11 +26,15 @@ class AboutScope < Neo::Koan
   def test_you_can_reference_nested_classes_using_the_scope_operator
     fido = Jims::Dog.new
     rover = Joes::Dog.new
-    assert_equal __, fido.identify
-    assert_equal __, rover.identify
+    assert_equal :jims_dog, fido.identify
+    assert_equal :joes_dog, rover.identify
+    #puts "fido.class is #{fido.class}"
+    #puts "rover.class is #{rover.class}"
+    #puts "Jims::Dog is #{Jims::Dog}"
+    #puts "Joes::Dog is #{Joes::Dog}"
 
-    assert_equal __, fido.class != rover.class
-    assert_equal __, Jims::Dog != Joes::Dog
+    assert_equal true, fido.class != rover.class
+    assert_equal true, Jims::Dog != Joes::Dog
   end
 
   # ------------------------------------------------------------------
@@ -39,15 +43,21 @@ class AboutScope < Neo::Koan
   end
 
   def test_bare_bones_class_names_assume_the_current_scope
-    assert_equal __, AboutScope::String == String
+    #puts "AboutScope::String is #{AboutScope::String}"
+    #puts "String is #{String}"
+    assert_equal true, AboutScope::String == String
   end
 
   def test_nested_string_is_not_the_same_as_the_system_string
-    assert_equal __, String == "HI".class
+    #puts "HI.class is #{"HI".class}"
+    #puts "String is #{String}"
+    assert_equal false, String == "HI".class
   end
 
   def test_use_the_prefix_scope_operator_to_force_the_global_scope
-    assert_equal __, ::String == "HI".class
+    #puts "HI.class is #{"HI".class}"
+    #puts "::String is #{::String}"
+    assert_equal true, ::String == "HI".class
   end
 
   # ------------------------------------------------------------------
@@ -55,7 +65,7 @@ class AboutScope < Neo::Koan
   PI = 3.1416
 
   def test_constants_are_defined_with_an_initial_uppercase_letter
-    assert_equal __, PI
+    assert_equal 3.1416, PI
   end
 
   # ------------------------------------------------------------------
@@ -63,17 +73,18 @@ class AboutScope < Neo::Koan
   MyString = ::String
 
   def test_class_names_are_just_constants
-    assert_equal __, MyString == ::String
-    assert_equal __, MyString == "HI".class
+    assert_equal true, MyString == ::String
+    assert_equal true, MyString == "HI".class
   end
 
-  def test_constants_can_be_looked_up_explicitly
-    assert_equal __, PI == AboutScope.const_get("PI")
-    assert_equal __, MyString == AboutScope.const_get("MyString")
+  def test_constants_can_be_looked_up_explicitly    
+    assert_equal true, PI == AboutScope.const_get("PI")
+    assert_equal true, MyString == AboutScope.const_get("MyString")
   end
 
   def test_you_can_get_a_list_of_constants_for_any_class_or_module
-    assert_equal __, Jims.constants
-    assert Object.constants.size > _n_
+    #puts "Object.constants.size is #{Object.constants.size}"
+    assert_equal [:Dog], Jims.constants
+    assert Object.constants.size > 0
   end
 end
